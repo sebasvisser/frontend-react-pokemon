09f24bc8387987c4eaab479c17c55f00165e1cc7
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-10-03 12:30:22 +0200

    jigglypuff opgehaald, bezig met sprites (image)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+
+
 
 function App() {
+
+    async function getPokemon(event){
+        try {
+            const pokemonName = event;
+            const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            console.log("Hier komt de pokemon")
+            console.log(result);
+        }
+        catch (error){
+            console.error(error);
+        }
+    }
+
+    const jiggly = getPokemon("jigglypuff");
+    const jigglyPicture = jiggly.data.sprites.front_shiny;
+    console.log("Sprite komt hier:");
+    console.log(jigglyPicture);
   return (
     <div>
+        <h1>JigglyPufff</h1>
       Begin hier met de opdracht!
+        {/*<img src={jigglyPicture} />*/}
     </div>
   );
 }
